2f1c4747ce5537a472d98a2e07401468394e5bf2
gantar22 <nathancarterwilliams@gmail.com>
2023-02-03 16:01:36 -0600

    committing

--- Assets/NotSlot/Scripts/Controller.cs
+++ Assets/NotSlot/Scripts/Controller.cs
@@ -3,42 +3,23 @@ public class Controller : MonoBehaviour
 {
     [SerializeField] private Camera m_Camera;
     [SerializeField] private Rigidbody m_RB;
-    [SerializeField] private float m_RollSpeed;
-    [SerializeField] private ControlMode m_Mode;
-
+    [SerializeField] private float m_RollSpeed = 10;
+    [SerializeField] private float m_Deadzone = .1f;
+    
     // Update is called once per frame
     void Update()
     {
         Vector3 movement = Vector3.zero;
-        if (m_Mode == ControlMode.WASD)
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        movement = (input.z * m_Camera.transform.forward) + (input.x * m_Camera.transform.right);
+
+        if (movement.magnitude > m_Deadzone)
         {
-            Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-            movement = (input.z * m_Camera.transform.forward) + (input.x * m_Camera.transform.right);
+            m_RB.velocity = movement * m_RollSpeed;
         }
         else
         {
-            movement = MouseInput();
+            m_RB.velocity -= m_RB.velocity * Time.deltaTime;
         }
-
-        m_RB.AddForce(movement * m_RollSpeed * Time.deltaTime);
-    }
-
-    private Vector3 MouseInput()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            Ray ray = m_Camera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray,out var hit) && hit.collider)
-            {
-                return (hit.point - m_RB.position).normalized;
-            }
-        }
-
-        return Vector3.zero;
-    }
-
-    private enum ControlMode
-    {
-        WASD,Mouse
     }
 }
