79932606504f300398a34c2ef12505bb75343361
lgarciaremes <luisagarciaremes@hotmail.com>
2023-02-03 17:14:08 -0700

    Added jump

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,29 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] private Rigidbody m_RB = default;
 
-    [SerializeField] private float m_Speed = 25f;
+    [SerializeField] private float m_Speed = 10f;
+    [SerializeField] private float m_Jump = 5f;
 
     [SerializeField] private float m_Deadzone = .1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    private bool m_CanJump = true;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && m_CanJump)
+        {
+            m_RB.velocity += new Vector3(0f, m_Jump, 0f);
+        }
+
         var input = Vector3.right * Input.GetAxis("Horizontal") + Vector3.forward * Input.GetAxis("Vertical");
-        if(input.magnitude > m_Deadzone)
-            m_RB.velocity = input * m_Speed;
+        if (input.magnitude > m_Deadzone)
+        {
+            m_RB.velocity = new Vector3(input.x * m_Speed,m_RB.velocity.y, input.z * m_Speed);
+        }
         else
         {
             m_RB.velocity -= m_RB.velocity * Time.deltaTime;
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            m_CanJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            m_CanJump = false;
+        }
+    }
+
 }
