a701ea8c91794776c81a641aeec0e605bd880ad5
lgarciaremes <luisagarciaremes@hotmail.com>
2023-02-05 10:17:20 -0700

    UI update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,42 +6,29 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager I;
+
     [SerializeField, Tooltip("In seconds")]
     private float m_GameTime;
-    [SerializeField]
-    private Animator m_HUDAnim;
-    [SerializeField]
-    private DualProgressBar m_Bar;
-    [SerializeField]
-    private Transform m_WinScreen;
-    [SerializeField]
-    private Transform m_LoseScreen;
-    [SerializeField, Tooltip("If assigned will pop up on loading")]
-    private Transform m_TutorialScreen;
     private bool m_GameOver = true;
+    public bool GameOver => m_GameOver;
     private float m_Timer;
-
     private Chunk[] m_Chunks = default;
-    
+
     private void Awake()
     {
         m_Chunks = GetComponentsInChildren<Chunk>();
-        if(m_TutorialScreen)
+        if (I == null)
         {
-            m_TutorialScreen.gameObject.SetActive(true);
-            Pause(true);
-        }
-        else
-        {
-            Commence();
+            I = this;
         }
     }
 
     public void Commence()
     {
-        if(m_GameOver)
+        if (m_GameOver)
         {
-            Pause(false);
+            UIManager.I.Pause(false);
 
             foreach (var chunk in m_Chunks)
             {
@@ -53,55 +40,17 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void CloseApp()
-    {
-        Application.Quit();
-    }
-
-    public void LoadScene(int inSceneID)
-    {
-        SceneManager.LoadScene(inSceneID);
-    }
-
     private void Update()
     {
-        if(!m_GameOver)
+        if (!m_GameOver)
         {
             if (m_Timer >= m_GameTime)
             {
-                m_LoseScreen.gameObject.SetActive(true);
-                m_HUDAnim.gameObject.SetActive(false);
+                UIManager.I.GameOver(false);
                 m_GameOver = true;
             }
             m_Timer += Time.deltaTime;
-            m_Bar.SetTime(m_Timer/m_GameTime);
+            UIManager.I.SetTime(m_Timer / m_GameTime);
         }
-
-        bool tutorialCheck = m_TutorialScreen && m_TutorialScreen.gameObject.activeSelf;
-
-        if(Input.GetKeyDown(KeyCode.Escape) && !m_GameOver && !tutorialCheck)
-        {
-            if(m_HUDAnim)
-            {
-                Pause(Time.timeScale == 0 ? false : true);
-            }
-            else
-            {
-                CloseApp();
-            }
-        }
-    }
-
-    public void Pause(bool inValue)
-    {
-        m_HUDAnim.SetBool("paused", inValue);
-       if (inValue)
-       {
-            Time.timeScale = 0;
-       }
-       else
-       {
-            Time.timeScale = 1;
-       }
     }
 }
